deb675b17ce94984f8afa954991f9497f700a82b
Iryna7ca <gorpinichvorona@gmail.com>
2023-11-25 18:37:34 +0100

    edded styles

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,7 +2,7 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
  
-import SimpleLightbox from "simplelightbox";
+ import SimpleLightbox from "simplelightbox";
 // Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.css";
  
