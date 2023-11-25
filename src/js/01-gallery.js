9c25f61e7f82826e5270dcedda458be2645bd5f8
Iryna7ca <gorpinichvorona@gmail.com>
2023-11-25 17:18:04 +0100

    task 01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,45 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+ 
+ 
+const gallery = document.querySelector(".gallery");
+const galleryMarkup = createGalleryItemsMarkup(galleryItems);
+gallery.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+ gallery.addEventListener('click', onGalleryItemClick);
+
+function createGalleryItemsMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+     <li class="gallery__item">
+     <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+       />
+     </a>
+     </li>
+     `;
+    })
+        .join("");
+      
+}
+  
+function onGalleryItemClick(event) {
+    event.preventDefault();
+    if (!event.target.classList.contains("gallery__image")) {
+        return;
+    }
+    console.log(event.target.dataset.source);
+    const originalSrc = event.target.dataset.source;
+     const instance = basicLightbox.create(`
+    <img src="${originalSrc}" width="800" height="600">
+`)
+
+    instance.show();
+      
+ 
+}
