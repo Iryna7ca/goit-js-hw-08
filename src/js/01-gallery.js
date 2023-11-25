da38e7baa6a59246c2de111ce2f766111dfe651e
Iryna7ca <gorpinichvorona@gmail.com>
2023-11-25 18:32:03 +0100

    simple

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,8 +1,11 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
-console.log(galleryItems);
+ 
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+ 
  
  
 const gallery = document.querySelector(".gallery");
